1d198a4b9d10eb5e9974dd8c2871cae475c1bba2
Rob Britton <rob@robbritton.com>
2018-03-14 15:25:14 -0600

    CI: Try merging gcloud install with image script.

--- install/build_mixer_docker.sh
+++ install/build_mixer_docker.sh
@@ -14,8 +14,39 @@ if [[ "${TARGET_DOCKER_IMAGE}" == "" ]]; then
 fi
 
 if [[ `command -v gcloud` == "" ]]; then
-  echo "gcloud not installed, please install it."
-  exit 1
+  if [[ "${INSTALL_GCLOUD}" == "1" ]]; then
+    if [[ $GCLOUD_SERVICE_KEY == "" ]]; then
+      echo "GCLOUD_SERVICE_KEY not set, please set it."
+      exit 1
+    fi
+
+    if [[ $GCP_PROJECT == "" ]]; then
+      echo "GCP_PROJECT not set, please set it."
+      exit 1
+    fi
+
+    echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > ${HOME}/gcloud-service-key.json
+
+    echo "Installing gcloud..."
+    wget -O /tmp/gcloud.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-193.0.0-linux-x86_64.tar.gz
+    sudo tar -zx -C /opt -f /tmp/gcloud.tar.gz
+
+    export PATH=$PATH:/opt/google-cloud-sdk/bin
+    # Need to ln so that `sudo gcloud` works
+    sudo ln -s /opt/google-cloud-sdk/bin/gcloud /usr/bin/gcloud
+
+    export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/gcloud-service-key.json
+
+    gcloud --quiet components update
+    gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+    gcloud config set project "${GCP_PROJECT}"
+
+    sudo gcloud components install docker-credential-gcr
+    docker-credential-gcr configure-docker
+  else
+    echo "gcloud not installed, please install it."
+    exit 1
+  fi
 fi
 
 if [[ `command -v docker` == "" ]]; then
