1ef0b1fb777f078bed439e6e46122af99b77ec55
Rob Britton <rob@robbritton.com>
2018-03-15 11:08:36 -0600

    CI: Re-added gcloud and GCR to image building. Set nightly to push every 10 minutes.

--- install/build_mixer_docker.sh
+++ install/build_mixer_docker.sh
@@ -28,6 +28,44 @@ if [[ `command -v docker` == "" ]]; then
   fi
 fi
 
+if [[ "${USE_GCLOUD}" == "1" && `command -v gcloud` == "" ]]; then
+  if [[ "${INSTALL_GCLOUD}" == "1" ]]; then
+    if [[ $GCLOUD_SERVICE_KEY == "" ]]; then
+      echo "GCLOUD_SERVICE_KEY not set, please set it."
+      exit 1
+    fi
+
+    if [[ $GCP_PROJECT == "" ]]; then
+      echo "GCP_PROJECT not set, please set it."
+      exit 1
+    fi
+
+    echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > ${HOME}/gcloud-service-key.json
+
+    echo "Installing gcloud..."
+    wget -O /tmp/gcloud.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-193.0.0-linux-x86_64.tar.gz
+    sudo tar -zx -C /opt -f /tmp/gcloud.tar.gz
+
+    export PATH=$PATH:/opt/google-cloud-sdk/bin
+    # Need to ln so that `sudo gcloud` works
+    sudo ln -s /opt/google-cloud-sdk/bin/gcloud /usr/bin/gcloud
+
+    export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/gcloud-service-key.json
+
+    gcloud --quiet components update
+    gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+    gcloud config set project "${GCP_PROJECT}"
+
+    sudo gcloud components install docker-credential-gcr
+    docker-credential-gcr configure-docker
+
+    docker login -u _json_key -p "$(cat ${HOME}/gcloud-service-key.json)" https://gco.io
+  else
+    echo "gcloud not installed, please install it."
+    exit 1
+  fi
+fi
+
 export ISTIO="${GOPATH}/src/istio.io"
 
 if [ ! -d "${ISTIO}/istio" ]; then
@@ -47,8 +85,10 @@ if [[ "${IMAGE_ID}" == "" ]]; then
 fi
 
 docker tag "${IMAGE_ID}" "${TARGET_DOCKER_IMAGE}"
-
-if [[ "${DOCKER_USERNAME}" != "" && "${DOCKER_PASSWORD}" != "" ]]; then
+if [[ "${USE_GCLOUD}" == "1" ]]; then
+  echo "Pushing to GCR..."
+  gcloud docker -- push "${TARGET_DOCKER_IMAGE}"
+elif [[ "${DOCKER_USERNAME}" != "" && "${DOCKER_PASSWORD}" != "" ]]; then
   echo "Pushing image to Docker Hub..."
   docker login --username "${DOCKER_USERNAME}" --password "${DOCKER_PASSWORD}"
   docker push "${TARGET_DOCKER_IMAGE}"
