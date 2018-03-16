61b6a53354b1ebe7ae7f1bb11cdb2654607ec5f1
Rob Britton <rob@robbritton.com>
2018-03-16 15:35:55 -0600

    CI: Added checks to see if binaries already exist.

--- install/install_docker.sh
+++ install/install_docker.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ `command -v docker` != "" ]]; then
+  echo "Docker already installed."
+  exit 0
+fi
+
 echo "Installing docker client..."
 VER=17.12.1
 wget -O /tmp/docker-$VER.tgz https://download.docker.com/linux/static/stable/x86_64/docker-$VER-ce.tgz || exit 1
--- install/install_gcloud.sh
+++ install/install_gcloud.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ `command -v gcloud` != "" ]]; then
+  echo "gcloud already installed."
+  exit 0
+fi
+
 echo "Installing gcloud..."
 wget -O /tmp/gcloud.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-193.0.0-linux-x86_64.tar.gz || exit 1
 sudo tar -zx -C /opt -f /tmp/gcloud.tar.gz
