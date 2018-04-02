e2aaca07f3dcae6ba20bfb086f3b591c978f5ee2
Scott Ganyo <scott@ganyo.com>
2018-04-02 16:10:27 -0700

    add doc line on ISTIO_VERSION var

--- install/local_install.sh
+++ install/local_install.sh
@@ -11,6 +11,7 @@
 #
 # Variables:
 # - INSTALL_PROTOC - installs protoc if set to 1. Used for CI.
+# - ISTIO_VERSION - set to appropriate Istio tag or branch to build Mixer from.
 
 DEFAULT_ISTIO_VERSION=0.7.1
 
