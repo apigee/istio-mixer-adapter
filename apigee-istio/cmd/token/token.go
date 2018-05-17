3215dc56c61e59fb15a09f4c9e22ade3610cdc4e
Scott Ganyo <theganyo@google.com>
2018-05-17 16:33:33 -0700

    add `token inspect` cmd
    Change-Id: I

--- apigee-istio/cmd/token/token.go
+++ apigee-istio/cmd/token/token.go
@@ -15,19 +15,25 @@
 package token
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-
+	"io/ioutil"
 	"net/http"
-
-	"bytes"
+	"os"
+	"time"
 
 	"github.com/apigee/istio-mixer-adapter/apigee-istio/shared"
+	"github.com/lestrrat/go-jwx/jwk"
+	"github.com/lestrrat/go-jwx/jws"
+	"github.com/lestrrat/go-jwx/jwt"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
 const (
 	tokenURLFormat         = "%s/token" // customerProxyURL
+	certsURLFormat         = "%s/certs" // customerProxyURL
 	clientCredentialsGrant = "client_credentials"
 )
 
@@ -35,20 +41,22 @@ type token struct {
 	*shared.RootArgs
 	id     string
 	secret string
+	file   string
 }
 
+// Cmd returns base command
 func Cmd(rootArgs *shared.RootArgs, printf, fatalf shared.FormatFn) *cobra.Command {
 	t := &token{RootArgs: rootArgs}
 
 	c := &cobra.Command{
 		Use:   "token",
-		Short: "OAuth Token Utilities",
-		Long:  "OAuth Token Utilities",
+		Short: "JWT Token Utilities",
+		Long:  "JWT Token Utilities",
 	}
 
 	c.AddCommand(cmdCreateToken(t, printf, fatalf))
-	//c.AddCommand(cmdVerifyToken(t, printf, fatalf))
-	//c.AddCommand(cmdDecodeToken(t, printf, fatalf))
+	c.AddCommand(cmdInspectToken(t, printf, fatalf))
+	//c.AddCommand(cmdRotateKey(t, printf, fatalf))
 
 	return c
 }
@@ -77,8 +85,47 @@ func cmdCreateToken(t *token, printf, fatalf shared.FormatFn) *cobra.Command {
 	return c
 }
 
-func (t *token) createToken(printf, fatalf shared.FormatFn) error {
+func cmdInspectToken(t *token, printf, fatalf shared.FormatFn) *cobra.Command {
+	c := &cobra.Command{
+		Use:   "inspect",
+		Short: "Inspect a JWT token",
+		Long:  "Inspect a JWT token",
+		Args:  cobra.NoArgs,
 
+		Run: func(cmd *cobra.Command, _ []string) {
+			err := t.inspectToken(printf, fatalf)
+			if err != nil {
+				fatalf("error inspecting token: %v", err)
+			}
+		},
+	}
+
+	c.Flags().StringVarP(&t.file, "file", "f", "", "token file (default: use stdin)")
+
+	return c
+}
+
+func cmdRotateKey(t *token, printf, fatalf shared.FormatFn) *cobra.Command {
+	c := &cobra.Command{
+		Use:   "rotate-key",
+		Short: "rotate private/public key",
+		Long:  "rotate private/public key",
+		Args:  cobra.NoArgs,
+
+		Run: func(cmd *cobra.Command, _ []string) {
+			err := t.rotateKey(printf, fatalf)
+			if err != nil {
+				fatalf("error creating token: %v", err)
+			}
+		},
+	}
+
+	c.Flags().StringVarP(&t.id, "kid", "k", "1", "new key id")
+
+	return c
+}
+
+func (t *token) createToken(printf, fatalf shared.FormatFn) error {
 	tokenReq := &tokenRequest{
 		ClientID:     t.id,
 		ClientSecret: t.secret,
@@ -103,6 +150,63 @@ func (t *token) createToken(printf, fatalf shared.FormatFn) error {
 	defer resp.Body.Close()
 
 	printf(tokenRes.Token)
+	return nil
+}
+
+func (t *token) inspectToken(printf, fatalf shared.FormatFn) error {
+	// Print JWT
+	var file = os.Stdin
+	if t.file != "" {
+		var err error
+		file, err = os.Open(t.file)
+		if err != nil {
+			fatalf("error opening file: %v", err)
+		}
+	}
+
+	jwtBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return errors.Wrap(err, "error reading jwt token")
+	}
+	token, err := jwt.ParseBytes(jwtBytes)
+	if err != nil {
+		return errors.Wrap(err, "error parsing jwt token")
+	}
+	jsonBytes, err := token.MarshalJSON()
+	var prettyJSON bytes.Buffer
+	err = json.Indent(&prettyJSON, jsonBytes, "", "\t")
+	if err != nil {
+		return errors.Wrap(err, "error printing jwt token")
+	}
+	printf(prettyJSON.String())
+
+	// verify JWT
+	printf("\nverifying...")
+
+	url := fmt.Sprintf(certsURLFormat, t.CustomerProxyURL)
+	jwkSet, err := jwk.FetchHTTP(url)
+	if err != nil {
+		fatalf("error fetching certs: %v", err)
+	}
+	_, err = jws.VerifyWithJWKSet(jwtBytes, jwkSet, nil)
+	if err != nil {
+		fatalf("certificate error: %v", err)
+	}
+	tokenURL := fmt.Sprintf(tokenURLFormat, t.CustomerProxyURL)
+	err = token.Verify(
+		jwt.WithAcceptableSkew(time.Minute),
+		jwt.WithAudience("istio"),
+		jwt.WithIssuer(tokenURL),
+	)
+	if err != nil {
+		fatalf("verification error: %v", err)
+	}
+
+	printf("token ok.")
+	return nil
+}
+
+func (t *token) rotateKey(printf, fatalf shared.FormatFn) error {
 	return nil
 }
 
