d41f705f26bc16bb019ec4c13ea33de545b6d3fb
Scott Ganyo <theganyo@google.com>
2018-12-17 10:46:10 -0800

    create standalone releasing doc
    Change-Id: Ib064015f7ab8f62309c9e27220ac13d07970d344

---
+++ RELEASING.md
@@ -0,0 +1,44 @@
+# Building a new draft release on Github
+
+1. set RELEASE env var
+    (eg. `RELEASE=1.0.0-alpha-2`)
+    
+2. create a release branch: `git checkout -b $RELEASE-prep`
+
+3. make release updates
+    1. update README.md to appropriate versions and instructions
+    2. update DEFAULT_ISTIO_VERSION in `bin/local_install.sh` to match Istio release
+    3. replace `samples/istio/istio-demo.yaml`, `istio-demo-auth.yaml`, `helloworld.yaml` from base Istio
+    4. update helloworld.yaml to include Istio sidecar: `istioctl kube-inject -f helloworld.yaml`
+    5. update `samples/istio/istio-demo.yaml`, `samples/istio/istio-demo-auth.yaml` mixer images to match $RELEASE
+    6. update version in `auth-proxy/apiproxy/policies/Send-Version.xml` to match $RELEASE
+    7. run `bin/build_proxy_resources.sh`
+
+4. Validate build
+    1. update `Gopkg.toml`, ensure appropriate version for `istio.io/istio`
+    2. update deps: `dep ensure --update`
+    3. remove existing istio from $GOPATH: `rm -rf $GOPATH/src/istio.io/istio`
+    4. build mixer: `bin/local_install.sh`
+
+5. Commit and push
+    1. verify your changes for git: `git status`
+    2. add and commit: `git commit -am "prep ${RELEASE}"`
+    3. tag the commit: `git tag ${RELEASE}`
+    4. push: `git push --set-upstream origin $RELEASE-prep ${RELEASE}`
+ (CircleCI will automatically build and tag docker image)
+
+6. verify the image
+    (gcr.io/apigee-api-management-istio/istio-mixer:$RELEASE)
+
+7. `bin/build_release.sh`
+    (creates a draft release on Github)
+
+8. edit Github release:
+    a. add mixer version and docker image URL to release notes
+    b. if this is not a pre-release, uncheck `This is a pre-release` checkbox
+
+9. submit PR for $RELEASE-prep branch
+
+10. merge and final verifications
+
+11. publish release on Github
--- bin/build_release.sh
+++ bin/build_release.sh
@@ -1,40 +1,7 @@
 #!/usr/bin/env bash
 
 # This script will build a new draft release on Github.
-# It should be used within the context of the process here:
-#
-#  1. set RELEASE env var
-#     (eg. `RELEASE=1.0.0-alpha-2`)
-#  2. create a release branch: `git checkout -b $RELEASE-prep`
-#  3. make release updates
-#     a. update README.md to appropriate versions and instructions
-#     b. update DEFAULT_ISTIO_VERSION in `bin/local_install.sh` to match Istio release
-#     c. replace `samples/istio/istio-demo.yaml`, `istio-demo-auth.yaml`, `helloworld.yaml` from base Istio
-#     d. update helloworld.yaml to include Istio sidecar: `istioctl kube-inject -f helloworld.yaml`
-#     e. update `samples/istio/istio-demo.yaml`, `samples/istio/istio-demo-auth.yaml` mixer images to match $RELEASE
-#     f. update version in `auth-proxy/apiproxy/policies/Send-Version.xml` to match $RELEASE
-#     g. run `bin/build_proxy_resources.sh`
-#  4. Validate build
-#     a. update `Gopkg.toml`, ensure appropriate version for `istio.io/istio`
-#     b. update deps: `dep ensure --update`
-#     e. remove existing istio from $GOPATH: `rm -rf $GOPATH/src/istio.io/istio`
-#     f. build mixer: `bin/local_install.sh`
-#  5. Commit and push
-#     a. verify your changes for git: `git status`
-#     b. add and commit: `git commit -am "prep ${RELEASE}"`
-#     c. tag the commit: `git tag ${RELEASE}`
-#     c. push: `git push --set-upstream origin $RELEASE-prep ${RELEASE}`
-#  (CircleCI will automatically build and tag docker image)
-#  5. verify the image
-#     (gcr.io/apigee-api-management-istio/istio-mixer:$RELEASE)
-#  6. `bin/build_release.sh`
-#     (creates a draft release on Github)
-#  7. edit Github release:
-#     a. add mixer version and docker image URL to release notes
-#     b. if this is not a pre-release, uncheck `This is a pre-release` checkbox
-#  8. submit PR for $RELEASE-prep branch
-#  9. merge and final verifications
-# 10. publish release on Github
+# See RELEASING.md for documentation of full release process.
 
 # use DRYRUN=1 to test build
 
