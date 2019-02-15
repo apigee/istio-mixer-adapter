ea6396679f233136e46cf04d882d155e0f1dc268
Scott Ganyo <scott@ganyo.com>
2019-02-15 13:50:59 -0800

    update gcloud version
    Change-Id: I407fd2ba737b44ca9ae3a33c4fc0eaec36a0798f

--- bin/install_gcloud.sh
+++ bin/install_gcloud.sh
@@ -9,7 +9,7 @@ if [[ `command -v gcloud` != "" ]]; then
 fi
 
 echo "Installing gcloud..."
-wget -O /tmp/gcloud.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-193.0.0-linux-x86_64.tar.gz || exit 1
+wget -O /tmp/gcloud.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-234.0.0-linux-x86_64.tar.gz || exit 1
 sudo tar -zx -C /opt -f /tmp/gcloud.tar.gz
 
 # Need to ln so that `sudo gcloud` works
