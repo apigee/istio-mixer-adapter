164ead3c1ad6b37856d6bf4f57cb2408fd9861ba
Scott Ganyo <scott@ganyo.com>
2019-05-10 14:22:27 -0700

    add script to update grpc definitions
    Change-Id: I9838503de3e5a5d2efba2b42d9080f61b5aaa8cd

---
+++ bin/build_grpc_definitions.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# This script will build the samples/apigee/grpc/definitions.yaml file.
+# Run this if any of the proto files (config, authorization, analytics) are changed.
+# See RELEASING.md for documentation of full release process.
+
+ISTIO_ROOT="${GOPATH-$HOME/go}/src/github.com/apigee/istio-mixer-adapter"
+MIXGEN=$ISTIO_ROOT/vendor/istio.io/istio/mixer/tools/mixgen/main.go
+DEFINITIONS_FILE="${ISTIO_ROOT}/samples/apigee/grpc/definitions.yaml"
+
+read -r -d '' DEFINITIONS_BASE <<"EOT"
+# This file generated via bin/build_grpc_definitions.sh. Regenerate if
+# any of the proto files (config, authorization, analytics) are changed.
+#
+# Defines the base structures and data map for the Apigee mixer adapter.
+# In general, these are static and should not need to be modified.
+# However, certain specific behaviors such as where to retrieve an API Key
+# could be changed here.
+---
+# instance for GRPC template authorization
+apiVersion: "config.istio.io/v1alpha2"
+kind: instance
+metadata:
+  name: apigee-authorization
+  namespace: istio-system
+spec:
+  template: apigee-authorization
+  params:
+    subject:
+      properties:
+        api_key: request.api_key | request.headers["x-api-key"] | ""
+        json_claims: request.auth.raw_claims | ""
+    action:
+      namespace: destination.namespace | "default"
+      service: api.service | destination.service.host | ""
+      path: api.operation | request.path | ""
+      method: request.method | ""
+---
+# instance for GRPC template analytics
+apiVersion: "config.istio.io/v1alpha2"
+kind: instance
+metadata:
+  name: apigee-analytics
+  namespace: istio-system
+spec:
+  template: apigee-analytics
+  params:
+    api_key: request.api_key | request.headers["x-api-key"] | ""
+    api_proxy: api.service | destination.service.host | ""
+    response_status_code: response.code | 0
+    client_ip: source.ip | ip("0.0.0.0")
+    request_verb: request.method | ""
+    request_uri: request.path | ""
+    useragent: request.useragent | ""
+    client_received_start_timestamp: request.time
+    client_received_end_timestamp: request.time
+    target_sent_start_timestamp: request.time
+    target_sent_end_timestamp: request.time
+    target_received_start_timestamp: response.time
+    target_received_end_timestamp: response.time
+    client_sent_start_timestamp: response.time
+    client_sent_end_timestamp: response.time
+    api_claims: # from jwt
+      json_claims: request.auth.raw_claims | ""
+---
+EOT
+
+
+templateDS=$GOPATH/src/istio.io/istio/mixer/template/authorization/template_handler_service.descriptor_set
+AUTHORIZATION=$(go run $MIXGEN template -d $templateDS -n apigee-authorization)
+
+templateDS=$GOPATH/src/github.com/apigee/istio-mixer-adapter/template/analytics/template_handler_service.descriptor_set
+ANALYTICS=$(go run $MIXGEN template -d $templateDS -n apigee-analytics)
+
+templateDS=$GOPATH/src/github.com/apigee/istio-mixer-adapter/adapter/config/config.proto_descriptor
+APIGEE=$(go run $MIXGEN adapter -c $templateDS -s=false -t apigee-authorization -t apigee-analytics -n apigee)
+
+echo "$DEFINITIONS_BASE $AUTHORIZATION $ANALYTICS $APIGEE" > $DEFINITIONS_FILE
+
+echo "Generated new file: $DEFINITIONS_FILE"
